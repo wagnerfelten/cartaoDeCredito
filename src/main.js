9ab3ecb8f548c469141303729a0cdaa49375d14b
Wagner Rodriges <felten19@gmail.com>
2022-10-19 19:00:12 -0300

    feito logica para diversas bandeiras de cartao

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
 import "./css/index.css"
+
+const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
+const ccBgColor02 = document.querySelector(".cc-bg svg > g g:nth-child(2) path")
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img")
+
+function setCardType(type) {
+  const colors = {
+    visa: ["#4959EC", "#8000CE"],
+    mastercard: ["#C69347", "#DF6F29"],
+    exercito: ["#994371", "#E8E8E8"],
+    default: ["black", "gray"],
+  }
+
+  ccBgColor01.setAttribute("fill", colors[type][0])
+  ccBgColor02.setAttribute("fill", colors[type][1])
+  ccLogo.setAttribute("src", `cc-${type}.svg`)
+}
+
+setCardType("exercito")
